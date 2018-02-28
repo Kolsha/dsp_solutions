31c9a9ca7e9b776752300b75f432c90a3c2daa15
Kolsha <ovchinnikovnikola@mail.ru>
2018-02-28 18:44:37 +0700

    passed

--- First Task/dft_new.py
+++ First Task/dft_new.py
@@ -32,48 +32,67 @@ def iDFT_slow(data):
     return out
 
 
-x1 = np.linspace(0, pi2 * 3/7, int(500 * 3/7))
-x2 = np.linspace(pi2 * 5/7, pi2, int(500 * 2/7))
+N = 500
+T = 1.0 # s
+Nyq = N / (2 * T)
+df = 1 / T
+f = 115.5 # Hz
+w = 2 * np.pi * f
+ 
+t = np.linspace(0, T, N)
+s = [np.sin(w * (_t if i < N * 3 / 5 else (_t + np.pi / 4)) / T) for i, _t in enumerate(t)]
+ 
+ 
+dft = iDFT_slow(s)
 
-x = np.linspace(0, pi2 * 5 / 7, 356)
-data = np.append(np.sin(x1), np.sin(x2))
+dft = np.fft.fftshift(dft)
+
+ndft = np.fft.ifft(s)
+
+
+plt.subplot(4, 1, 1)
+
+plt.plot(t, s, label='data')
+
+
+plt.subplot(4, 1, 2)
+
+plt.plot(t, np.angle(dft), label='amplitude')
+
+#plt.plot(t, np.absolute(ndft), label='N amplitude')
+
+#plt.legend(loc=(0.1, 0.25))
+#plt.show()
+
+#differ result 
+
+
+
+
+
+
+x = np.linspace(0, pi2, 2000)
+data = (np.sin(100 * x) + np.sin(101.8 * x)) / np.sqrt(2)
 
 dft = iDFT_slow(data)
+dft = np.fft.fftshift(dft)
+#ndft = np.fft.ifft(data);
 
-ndft = np.fft.ifft(data);
-
+#plt.subplot(3, 1, 1)
+plt.subplot(4, 1, 3)
 plt.plot(x, data, label='data')
-plt.plot(x, np.absolute(dft), label='amplitude')
-
-plt.plot(x, np.absolute(ndft), label='N amplitude')
-
-plt.legend(loc=(0.1, 0.25))
-plt.show()
+plt.subplot(4, 1, 4)
+plt.plot(x, np.angle(dft), label='amplitude')
 
 
-
+#plt.subplot(3, 1, 2)
+#plt.plot(x, np.absolute(np.subtract(np.absolute(ndft), np.absolute(dft))), label='amplitude')
+#plt.plot(x, np.absolute(ndft), label='N amplitude')
 
 
 
 
-x = np.linspace(0, pi2, 500)
-data = (np.sin(100 * x) + np.sin(101 * x)) / np.sqrt(2)
-
-dft = iDFT_slow(data)
-ndft = np.fft.ifft(data);
-
-plt.subplot(3, 1, 1)
-plt.plot(x, data, label='data')
-plt.plot(x, np.absolute(dft), label='amplitude')
-
-
-plt.subplot(3, 1, 2)
-plt.plot(x, np.absolute(dft), label='amplitude')
-plt.subplot(3, 1, 3)
-plt.plot(x, np.absolute(ndft), label='N amplitude')
-
-
-plt.legend(loc=(0.4, 0.75))
+#plt.legend(loc=(0.4, 0.75))
 plt.show()
 
 
