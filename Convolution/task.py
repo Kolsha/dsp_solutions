caad8863f9512680ce1fa8cb726e1ca24a46a199
Kolsha <ovchinnikovnikola@mail.ru>
2018-03-08 11:11:59 +0700

    Convolution complete

--- Convolution/task.py
+++ Convolution/task.py
@@ -5,46 +5,12 @@ import numpy as np
 
 pi2 = np.pi * 2.0
 
-def DFT_slow(data):
-    n = len(data)
-    out = []
-    for k in range(n):
-        v = np.imag(0)
-        v.flags['WRITEABLE'] = True
-        for t in range(n):
-            angle = 1j * pi2 * t * k / n
-            a = data[t] * np.exp(-angle)
-            v = v + a
-        out.append(v)
-    return out
 
-def iDFT_slow(data):
-    n = len(data)
-    out = []
-    for k in range(n):
-        v = np.imag(0)
-        v.flags['WRITEABLE'] = True
-        for t in range(n):
-            angle = 1j * pi2 * t * k / n
-            a = data[t] * np.exp(angle)
-            v = v + a
-        out.append(v / n)
-    return out
-
-
-def fConv_slow(f1, f2):
-    dft1 = DFT_slow(f1)
-    dft2 = DFT_slow(f2)
-    l1 = len(dft1)
-    l2 = len(dft2)
-
-    n = max(l1, l2)
-    if(n > l1):
-        np.pad(dft1, (0, n), 'constant', constant_values=0)
-    if(n > l2):
-        np.pad(dft2, (0, n), 'constant', constant_values=0)
-
-    return iDFT_slow(np.multiply(dft1, dft2))
+def fConv(f1, f2):
+    dft1 = np.fft.fft(f1)
+    dft2 = np.fft.fft(f2)
+    n = min(len(dft1), len(dft2))
+    return np.fft.ifft(dft1[:n] * dft2[:n])
 
 def Conv_slow(f1, f2):
     n = len(f1)
@@ -65,30 +31,62 @@ def Conv_slow(f1, f2):
 
 
 
-x = np.linspace(0, 2*pi2, 20)
-data1 = (np.sin(1 * x[0:10])) / np.sqrt(2)
+x = np.linspace(0, (2 * pi2), 640)
+data1 = (np.sin(1 * x)) / np.sqrt(2)
 
-data2 = (np.sin(20.8 * x)) / np.sqrt(2)
+data2 = (np.sin(1 * x + np.pi / 2.0)) / np.sqrt(2)
 
 
-conv1 = Conv_slow(data1, data2)
-conv2 = fConv_slow(data1, data2)
+Sconv = Conv_slow(data1, data2)
+Fconv = fConv(data1, data2)
+NPconv = np.convolve(data1, data2, mode='same')
 
-plt.subplot(4, 1, 1)
+#print len(x), len(NPconv)
 
+numPlots = 5
+
+plt.subplot(numPlots, 1, 1).set_title('Datas')
 plt.plot(x, [d.real for d in data1], label='data1')
-plt.subplot(4, 1, 2)
 plt.plot(x, [d.real for d in data2], label='data2')
+plt.legend(loc='upper right')
 
 
-plt.subplot(4, 1, 3)
 
-plt.plot(x, [d.real for d in conv1], label='conv')
+plt.subplot(numPlots, 1, 2).set_title('Slow and Furie')
+plt.plot(x, [d.real for d in Sconv], label='Slow conv')
+plt.plot(x, [d.real for d in Fconv], label='Fconv')
+plt.legend(loc='upper right')
 
 
-plt.subplot(4, 1, 4)
+plt.subplot(numPlots, 1, 3).set_title('Slow and NumPy')
+plt.plot(x, [d.real for d in Sconv], label='Slow conv')
+plt.plot(x, [d.real for d in NPconv], label='NPconv')
+plt.legend(loc='upper right')
 
-plt.plot(x, [d.real for d in np.subtract(conv1, conv2)], label='fconv')
+
+plt.subplot(numPlots, 1, 4).set_title('Furie and NumPy')
+plt.plot(x, [d.real for d in Fconv], label='Fconv')
+plt.plot(x, [d.real for d in NPconv], label='NPconv')
+plt.legend(loc='upper right')
+
+
+plt.subplot(numPlots, 1, 5).set_title('Deltas')
+plt.plot(x, [d.real for d in np.subtract(Sconv, Fconv)], label='Sl. & Furie')
+plt.plot(x, [d.real for d in np.subtract(Sconv, NPconv)], label='Sl. & NP')
+plt.plot(x, [d.real for d in np.subtract(Fconv, NPconv)], label='Furie. & NP')
+
+plt.legend(loc='upper right')
+
+'''plt.subplot(6, 1, 5).set_title('Delta Slow and Furie')
+plt.plot(x, [d.real for d in np.subtract(Sconv, Fconv)], label='delta')
+
+
+plt.subplot(6, 1, 6).set_title('Delta Slow and NumPy')
+plt.plot(x, [d.real for d in np.subtract(Sconv, NPconv)], label='delta')
+'''
+
+
+#plt.ylim(-1.5, 2.0)
 
 
 plt.show()
